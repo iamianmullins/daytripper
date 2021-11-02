40aa13a80b3cbea43c707cccfb0909fc84cdca72
Ian Mullins <mullins.89@hotmail.com>
2021-11-02 23:01:39 +0000

    Day Trip List Display view implementation, menu and action bar implementation

--- app/src/main/java/org/wit/daytripper/activities/DayTripperActivity.kt
+++ app/src/main/java/org/wit/daytripper/activities/DayTripperActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import org.wit.daytripper.databinding.ActivityDaytripperBinding
+import org.wit.daytripper.main.MainApp
 import org.wit.daytripper.models.DayTripperModel
 import timber.log.Timber
 import timber.log.Timber.i
@@ -13,28 +14,27 @@ class DayTripperActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDaytripperBinding
     var dayTrip = DayTripperModel()
-    val dayTrips = ArrayList<DayTripperModel>()
-
+    lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityDaytripperBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        Timber.plant(Timber.DebugTree())
-
+        app = application as MainApp
         i("Day Trip Activity started...")
-
         //Add
         binding.btnAdd.setOnClickListener() {
             dayTrip.title = binding.dayTripTitle.text.toString()
             dayTrip.description = binding.description.text.toString()
             if (dayTrip.title.isNotEmpty()) {
-                dayTrips.add(dayTrip.copy())
-                i("add Button Pressed: $dayTrip")
-                for (i in dayTrips.indices)
-                { i("DayTrip[$i]:${this.dayTrips[i]}") }
+                app.dayTrips.add(dayTrip.copy())
+                i("add Button Pressed: ${dayTrip}")
+                for (i in app.dayTrips.indices)
+                    { i("DayTrip[$i]:${app.dayTrips[i]}")
+                }
+                setResult(RESULT_OK)
+                finish()
             }
             else {
                 Snackbar
