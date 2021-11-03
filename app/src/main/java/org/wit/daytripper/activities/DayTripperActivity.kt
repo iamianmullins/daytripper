4cdf4634229ab60cd32ffadf1ac81064db896cda
Ian Mullins <mullins.89@hotmail.com>
2021-11-03 00:50:35 +0000

    Day trip mem store implementation, day trip update functionality

--- app/src/main/java/org/wit/daytripper/activities/DayTripperActivity.kt
+++ app/src/main/java/org/wit/daytripper/activities/DayTripperActivity.kt
@@ -2,7 +2,10 @@ package org.wit.daytripper.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.google.android.material.snackbar.Snackbar
+import org.wit.daytripper.R
 import org.wit.daytripper.databinding.ActivityDaytripperBinding
 import org.wit.daytripper.main.MainApp
 import org.wit.daytripper.models.DayTripperModel
@@ -14,33 +17,54 @@ class DayTripperActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDaytripperBinding
     var dayTrip = DayTripperModel()
+    var edit = false
     lateinit var app: MainApp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityDaytripperBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbarAdd.title = title
+        setSupportActionBar(binding.toolbarAdd)
 
         app = application as MainApp
         i("Day Trip Activity started...")
+
+        if (intent.hasExtra("daytrip_edit")) {
+            edit = true
+            dayTrip = intent.extras?.getParcelable("daytrip_edit")!!
+            binding.dayTripTitle.setText(dayTrip.title)
+            binding.description.setText(dayTrip.description)
+            binding.btnAdd.setText(R.string.save_daytrip)
+        }
+
         //Add
         binding.btnAdd.setOnClickListener() {
             dayTrip.title = binding.dayTripTitle.text.toString()
             dayTrip.description = binding.description.text.toString()
-            if (dayTrip.title.isNotEmpty()) {
-                app.dayTrips.add(dayTrip.copy())
-                i("add Button Pressed: ${dayTrip}")
-                for (i in app.dayTrips.indices)
-                    { i("DayTrip[$i]:${app.dayTrips[i]}")
-                }
-                setResult(RESULT_OK)
-                finish()
-            }
-            else {
-                Snackbar
-                    .make(it,"Please Enter a day trip name", Snackbar.LENGTH_LONG)
+            if (dayTrip.title.isEmpty()) {
+                Snackbar.make(it,R.string.enter_daytrip_title, Snackbar.LENGTH_LONG)
                     .show()
+            } else {
+                if (edit) {
+                    app.dayTrips.update(dayTrip.copy())
+                } else {
+                    app.dayTrips.create(dayTrip.copy())
+                }
             }
+            setResult(RESULT_OK)
+            finish()
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_daytrip, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_cancel -> { finish() }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/org/wit/daytripper/models/DayTripMemStore.kt
@@ -0,0 +1,37 @@
+package org.wit.daytripper.models
+
+import timber.log.Timber.i
+
+var lastId = 0L
+
+internal fun getId(): Long {
+    return lastId++
+}
+
+class DayTripMemStore: DayTripStore {
+
+    val daytrips = ArrayList<DayTripperModel>()
+
+    override fun findAll(): List<DayTripperModel> {
+        return daytrips
+    }
+
+    override fun create(dayTrip: DayTripperModel) {
+        dayTrip.id = getId()
+        daytrips.add(dayTrip)
+        logAll()
+    }
+
+    override fun update(dayTrip: DayTripperModel) {
+        var foundDayTrip: DayTripperModel? = daytrips.find { p -> p.id == dayTrip.id }
+        if (foundDayTrip != null) {
+            foundDayTrip.title = dayTrip.title
+            foundDayTrip.description = dayTrip.description
+            logAll()
+        }
+    }
+
+    fun logAll() {
+        daytrips.forEach{ i("${it}") }
+    }
+}
